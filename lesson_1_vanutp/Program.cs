ddbd5cf9ce1a9ca1b33bc90e909c381385df8b0d
gravity <dvaerkagama@gmail.com>
2023-08-11 18:00:28 +0300

    add name

--- lesson_1_vanutp/Program.cs
+++ lesson_1_vanutp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What your name?");
+        Console.WriteLine(Console.ReadLine());
+    }
+}
